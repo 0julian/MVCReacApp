ea92b289faf19f3bcfc7eb322bf683e701188099
Usuario <Usuario@DESKTOP-RH130P1>
2023-12-09 03:10:05 -0600

    Agregar archivos de proyecto.

---
+++ MVCReacApp/ClientApp/src/components/Tarjeta.js
@@ -0,0 +1,13 @@
+﻿const Tarjeta = (props) => {
+    return (
+        <div class="card text-center bg-dark mt-5">
+            <div class="card-body">
+                <h1 className="card-title text-info">{ props.titulo || "Titulo de la tarjeta"}</h1>
+                <p className="card-text text-light">{props.descripcion || "Descripcion de la tarjeta"}</p>
+                <button className="btn btn-danger">{ props.textbutton || "Click aqui"}</button>
+            </div>
+        </div>
+    )
+}
+
+export default Tarjeta
